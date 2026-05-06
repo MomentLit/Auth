73750c7d50459c9b145486fdbf6edf386b262ec9
hyun731 <gilhyun.gwon0731@gmail.com>
2026-05-07 08:32:40 +0900

    delete: 쓸모없는 파일 삭제

--- src/main/java/com/example/auth/global/config/RedisConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.auth.global.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-
-@Configuration
-@EnableRedisRepositories
-public class RedisConfig {
-}

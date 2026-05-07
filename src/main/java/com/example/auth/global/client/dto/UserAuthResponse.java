c7a2f8a782cc5383cb82461528be2693413b9633
hyun731 <gilhyun.gwon0731@gmail.com>
2026-05-07 17:26:58 +0900

    feat: UserAuthResponse에서 단일 역할만 받을 수 있도록 수정

--- src/main/java/com/example/auth/global/client/dto/UserAuthResponse.java
+++ src/main/java/com/example/auth/global/client/dto/UserAuthResponse.java
@@ -1,11 +1,9 @@
 package com.example.auth.global.client.dto;
 
-import java.util.List;
 import java.util.UUID;
 
 public record UserAuthResponse(
         UUID userId,
         String name,
         String role,
-        List<String> roles
 ) {}

8d06a748d01fde8df2f21a9c0ca3da150cd4d890
주환 <jhlarry1109@gmail.com>
2026-07-03 10:40:01 +0900

    fix: exception 더 상세하게

--- src/main/java/com/example/auth/global/client/UserServiceClient.java
+++ src/main/java/com/example/auth/global/client/UserServiceClient.java
@@ -4,8 +4,10 @@ import com.example.auth.dto.request.SignInRequest;
 import com.example.auth.global.client.dto.response.GoogleUserInfoResponse;
 import com.example.auth.global.client.dto.response.UserAuthResponse;
 import com.example.auth.global.client.dto.request.UserGoogleOauthRequest;
+import com.example.auth.global.exception.UnauthorizedException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestClient;
 
 @Component
@@ -22,11 +24,16 @@ public class UserServiceClient {
     }
 
     public UserAuthResponse authenticate(SignInRequest request) {
-        return restClient.post()
-                .uri("/internal/users/authenticate")
-                .body(request)
-                .retrieve()
-                .body(UserAuthResponse.class);
+        try {
+            return restClient.post()
+                    .uri("/internal/users/authenticate")
+                    .body(request)
+                    .retrieve()
+                    .body(UserAuthResponse.class);
+
+        } catch (HttpClientErrorException.Unauthorized e) {
+            throw new UnauthorizedException("이메일 또는 비밀번호가 일치하지 않습니다.");
+        }
     }
 
     public UserAuthResponse authenticateGoogle(GoogleUserInfoResponse request) {
